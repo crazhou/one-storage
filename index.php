3ad2dfc6461f3f71a3152747818b5c9cc6b46980
xiaoxi <crazhou@163.com>
2017-10-22 23:31:37 +0800

    删除非必要文件并更新消息示例

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
 <?php
-require 'vendor/autoload.php';
-
-use Upyun\Config;
-use Upyun\Signature;
-
-$bucketConfig = new Config('miniprogram', 'admin' , 'xdxd1078' );
-
-
-// echo md5('Apple', FALSE);
-$str = 'Hello UPYUN';
-$str2 = base64_encode($str);
-$arr1 = hash_algos();
-
-var_dump(md5_file('43455.jpg'));
-// var_dump(strlen($str), strlen($str2), $str2);
--- wxtool.js
+++ wxtool.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 const xml2js = require('xml2js');
 const path = require('path');
-
+/**
+ * error code 说明.
+ * <ul>
+ *    <li>-40001: 签名验证错误</li>
+ *    <li>-40002: xml解析失败</li>
+ *    <li>-40003: sha加密生成签名失败</li>
+ *    <li>-40004: encodingAesKey 非法</li>
+ *    <li>-40005: appid 校验错误</li>
+ *    <li>-40006: aes 加密失败</li>
+ *    <li>-40007: aes 解密失败</li>
+ *    <li>-40008: 解密后得到的buffer非法</li>
+ *    <li>-40009: base64加密失败</li>
+ *    <li>-40010: base64解密失败</li>
+ *    <li>-40011: 生成xml失败</li>
+ * </ul>
+ */
 const parser = new xml2js.Parser();
-
 function parseEnXML(filename, fn) {
     fs.readFile(path.resolve(__dirname, filename), (err, data) => {
         if(err==null) {
@@ -28,6 +42,6 @@ function parseEnXML(filename, fn) {
 }
 
 
-parseEnXML('data/event.xml', (err, data) => {
+parseEnXML('data/img.xml', (err, data) => {
     console.log('OK->', err, data);
 })
